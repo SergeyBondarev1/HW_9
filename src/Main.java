b169e409b21539970c6084e1c124601d12a7e51a
Сергей <bondarevser.bondarev@yandex.ru>
2023-05-11 12:58:00 +0300

    3 tasks

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("сумма трат за месяц составляет " + sum + " рублей.");
+    }
+
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int minSalary = 200001;
+        int maxSalary = 99999;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minSalary) {
+                minSalary = arr[i];
+            }
+        }
+        System.out.println("минимальная зарплата "+minSalary+" рублей.");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSalary) {
+                maxSalary = arr[i];
+            }
+        }
+        System.out.println("максимальная зарплата "+maxSalary+" рублей.");
+    }
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i]/30;
+        }
+        System.out.println("средняя сумма трат за месяц составляет " + sum + " рублей.");
+    }
+
+}
